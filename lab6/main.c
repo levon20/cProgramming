4a81d45a47af6a461886fd3d1eea074629871682
agtti <levsidorov2016@gmail.com>
2026-04-24 19:33:27 +1000

    Added solution task 6

--- lab6/main.c
+++ lab6/main.c
@@ -8,6 +8,10 @@ struct Human
 	int year;
 };
 
+void ArrayCpy(struct Human*, struct Human*, int);
+void BubleSort(struct Human*, int);		
+
+
 void main()
 {
 	int len = 4;
@@ -17,31 +21,44 @@ void main()
 
 	for(int i = 0; i < len; i++)
 	{
-		scanf("%20s %20s %d", &listHum0[i].name, &listHum0[i].surname, &listHum0.year);
-		listHum0[i].year >= 0;
+		scanf("%20s %20s %d", listHum0[i].name, listHum0[i].surname, &(listHum0[i].year));
+		if(listHum0[i].year < 0) listHum0[i].year = 0;
 	}
 
+	ArrayCpy(listHum0, listHum1, len);
 
+	BubleSort(listHum1, len);
+
+	for(int i = 0; i < len; i++)
+		printf("%d. %s %s %d \n", i, listHum1[i].name, listHum1[i].surname, listHum1[i].year);
+}
+
+void ArrayCpy(struct Human* originList, struct Human* newList, int len )
+{
+	for(int i = 0; i < len; i++)
+		newList[i] = originList[i];
 }
 
 void BubleSort(struct Human* list, int len)
 {
-	int flag = 0;
+	int flag = 1;
 	struct Human boofer;
+
 	while(flag)
 	{
-		for(int i = 0; i < len; i++)
+		flag = 0;
+
+		for(int i = 0; i < len-1; i++)
 		{
 			if(list[i].year > list[i+1].year)
 			{
 				boofer = list[i];
 				list[i] = list[i+1];
 				list[i+1] = boofer;
-				flag = 0;
+				flag = 1;
 			}
-			else flag = 1;
 		}
-		if(flag) return;	
+
+		len -= 1;	
 	}
-	
 }
